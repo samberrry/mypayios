794df4fbeddac5c6dae7e5625900f5c231dfd2b6
Hessam Modaberi <hessam.modaberi@gmail.com>
2016-12-09 01:24:18 +0330

    classes for table view and main view

---
+++ QRScannerVC.swift
@@ -0,0 +1,35 @@
+//
+//  QRScannerVC.swift
+//  mypay
+//
+//  Created by Hessam on 12/9/16.
+//  Copyright © 2016 Hessam. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
