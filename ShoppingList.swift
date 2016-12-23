af7c40ab1d84a311ea4777807b28b0443355f613
Hessam Modaberi <hessam.modaberi@gmail.com>
2016-12-23 19:29:11 +0330

    new-changes

--- ShoppingList.swift
+++ ShoppingList.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class MainTableVC: UITableViewController {
+class ShoppingList: UIViewController,UITableViewDelegate,UITableViewDataSource{
 
+    //MARK: Properties
+    @IBOutlet weak var tableView: UITableView!
     static var goodsList = [Goods]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,20 +38,20 @@ class MainTableVC: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return MainTableVC.goodsList.count
+        return ShoppingList.goodsList.count
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MainMenuCell", for: indexPath) as! MainMenuCell
-        let item = MainTableVC.goodsList[indexPath.row]
+        let item = ShoppingList.goodsList[indexPath.row]
         // Configure the cell...
         cell.labelName.text = item.name
         cell.labelPrice.text = "Price: \(String(item.price))$"
@@ -59,16 +61,16 @@ class MainTableVC: UITableViewController {
     }
  
     // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
     
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            MainTableVC.goodsList.remove(at: indexPath.row)
+            ShoppingList.goodsList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -76,15 +78,15 @@ class MainTableVC: UITableViewController {
     }
     
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    //  to support rearranging the table view.
+     func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
     */
 
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    //  to support conditional rearranging of the table view.
+     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
@@ -94,7 +96,7 @@ class MainTableVC: UITableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
