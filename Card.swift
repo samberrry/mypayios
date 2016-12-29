9fd8d5cf36455641a81b00679907c0df95e72bc4
Hessam Modaberi <hessam.modaberi@gmail.com>
2016-12-29 20:04:48 +0330

    card path change

--- Card.swift
+++ Card.swift
@@ -36,7 +36,7 @@ class Card: NSObject,NSCoding{
     }
     //MARK: Archiving Paths
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
-    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("appcards")
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("mycards")
     
     //MARK: NSCoding
     func encode(with aCoder: NSCoder) {
